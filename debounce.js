f812e29684c22adbaabeb241297eee6947a038c1
BJJIN <17856419249@163.com>
2022-04-17 16:53:47 +0800

    add note

--- debounce.js
+++ debounce.js
@@ -1,3 +1,9 @@
+/**
+ * 防抖：归一个固定时间如果固定时间没有在操作，就执行一次
+ * @param fn
+ * @param delay
+ * @returns {(function(...[*]): void)|*}
+ */
 const debounce = (fn, delay) => {
   let timer = null;
   return function (...args) {
--- throttle.js
+++ throttle.js
@@ -1,3 +1,10 @@
+/**
+ * 节流：一段时间执行一次
+ * @param fn
+ * @param delay
+ * @returns {(function(...[*]): void)|*}
+ */
+
 const throttle = (fn, delay) => {
     let timer = null;
     return function (...arg) {
