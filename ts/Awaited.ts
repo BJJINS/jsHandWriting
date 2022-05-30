9462dea668c58360bfabd7594317cc2e31e32423
BJJIN <17856419249@163.com>
2022-05-31 07:56:12 +0800

    Awaited

---
+++ ts/Awaited.ts
@@ -0,0 +1,12 @@
+//假如我们有一个 Promise 对象，这个 Promise 对象会返回一个类型。在 TS 中，
+// 我们用 Promise 中的 T 来描述这个 Promise 返回的类型。请你实现一个类型，可以获取这个类型。
+// 比如：Promise<ExampleType>，请你返回 ExampleType 类型。
+type a = Promise<number[]>;
+type c = Awaited<string>;
+type MyAwaited<T> = T extends null
+  ? T
+  : T extends Object & { then: (onfulfilled: infer F) => any }
+  ? F extends (value: infer V, ...args) => any
+    ? MyAwaited<V>
+    : never
+  : T;
