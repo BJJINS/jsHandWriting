e479ba0e4ced77678c01ea2259520afe27a9edf0
BJJIN <17856419249@163.com>
2022-07-03 10:35:02 +0800

    限制并发数

--- myPromise/limitPromise.js
+++ myPromise/limitPromise.js
@@ -1,27 +1,24 @@
-function limit(count, array, iterateFunc) {
-  const tasks = [];
-  const doingTasks = [];
-  let i = 0;
-  const enqueue = () => {
-    if (i === array.length) {
-      return Promise.resolve();
+const asyncPool = async (limit, array) => {
+  if (array.length === 0) {
+    return Promise.resolve([]);
+  }
+  const ret = [];
+  const executing = [];
+  for (let i = 0; i < array.length; i++) {
+    const item = Promise.resolve(array[i]);
+    ret.push(item);
+    if (array.length >= limit) {
+      executing.push(item);
+      item.then(() => executing.splice(executing.indexOf(item), 1));
+      if (executing.length >= limit) {
+        await Promise.race(executing);
+      }
     }
-    const task = Promise.resolve().then(() => iterateFunc(array[i++]));
-    tasks.push(task);
-    const doing = task.then(() =>
-      doingTasks.splice(doingTasks.indexOf(doing), 1)
-    );
-    doingTasks.push(doing);
-    const res =
-      doingTasks.length >= count ? Promise.race(doingTasks) : Promise.resolve();
-    return res.then(enqueue);
-  };
-  return enqueue().then(() => Promise.all(tasks));
-}
+  }
+  return Promise.allSettled(ret);
+};
 
-// test
-const timeout = (i) =>
-  new Promise((resolve) => setTimeout(() => resolve(i), i));
-limit(2, [1000, 1000, 1000, 1000], timeout).then((res) => {
-  console.log(res);
+const a = asyncPool(2, [1000, 5000, 3000, 2000]);
+a.then((value) => {
+  console.log("sssssssss", value);
 });
