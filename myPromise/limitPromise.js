d4f841cf2f6f9ab11940661967e41fd7744ca52d
金保佳 <17856419249@163.com>
2022-08-22 14:50:11 +0800

    limit promise

--- myPromise/limitPromise.js
+++ myPromise/limitPromise.js
@@ -1,25 +1,20 @@
-const asyncPool = (limit, array) => {
-  if (array.length === 0) {
-    return Promise.resolve([]);
-  }
-  
-  const ret = [];
-  const executing = [];
-  for (let i = 0; i < array.length; i++) {
-    const item = array[i];
-    ret.push(item);
-    if (array.length >= limit) {
-      const p = item.then(() => executing.splice(executing.indexOf(item), 1));
-      executing.push(p);
-      if (executing.length >= limit) {
-        await Promise.race(executing);
+async function asyncPool(poolLimit, array, iteratorFn) {
+  const ret = []; // 存储所有的异步任务
+  const executing = []; // 存储正在执行的异步任务
+  for (const item of array) {
+    // 调用iteratorFn函数创建异步任务
+    const p = Promise.resolve().then(() => iteratorFn(item, array));
+    ret.push(p); // 保存新的异步任务
+
+    // 当poolLimit值小于或等于总任务个数时，进行并发控制
+    if (poolLimit <= array.length) {
+      // 当任务完成后，从正在执行的任务数组中移除已完成的任务
+      const e = p.then(() => executing.splice(executing.indexOf(e), 1));
+      executing.push(e); // 保存正在执行的异步任务
+      if (executing.length >= poolLimit) {
+        await Promise.race(executing); // 等待较快的任务执行完成
       }
     }
   }
-  return Promise.allSettled(ret);
-};
-
-const a = asyncPool(2, [1000, 5000, 3000, 2000]);
-a.then((value) => {
-  console.log("sssssssss", value);
-});
+  return Promise.all(ret);
+}
